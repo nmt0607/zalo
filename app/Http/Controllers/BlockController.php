7fc7896107b4e3dfbdbf379177fc3c2f68f59a56
Hoangcongtue99 <hoangcongtue99@gmail.com>
2021-11-28 18:43:16 +0700

    set_block_diary

--- app/Http/Controllers/BlockController.php
+++ app/Http/Controllers/BlockController.php
@@ -6,6 +6,7 @@ use App\Exceptions\AlreadyBlockException;
 use App\Exceptions\AlreadyUnblockException;
 use App\Exceptions\CantSelfBlockException;
 use App\Exceptions\UserNotExistedException;
+use App\Http\Requests\SetBlockDiaryRequest;
 use App\Http\Requests\SetBlockUserRequest;
 use App\Models\Relationship;
 use App\Models\User;
@@ -38,6 +39,51 @@ class BlockController extends Controller
                     'from_id' => auth()->id(),
                     'to_id' => $user_id,
                     'status' => '3',
+                ]);
+            }
+            else {
+                throw new AlreadyBlockException();
+            }
+        }
+        else {
+            if ($block) {
+                $block->delete();
+            }
+            else {
+                throw new AlreadyUnblockException();
+            }
+        }
+
+        return response()->json([
+            'code' => config('response_code.ok'),
+            'message' => __('messages.ok'),
+        ]);
+    }
+
+    public function set_block_diary(SetBlockDiaryRequest $request)
+    {
+        $user_id = $request->user_id;
+        $type = $request->type;
+
+        if ($user_id == auth()->id()) {
+            throw new CantSelfBlockException();
+        }
+
+        try {
+            $user = User::findOrFail($user_id);
+        } catch (ModelNotFoundException $e) {
+            throw new UserNotExistedException();
+        }
+
+        
+        $block = Relationship::where('from_id', auth()->id())->where('to_id', $user_id)->where('status', 4)->first();
+        
+        if ($type == 1) {   
+            if (!$block) {
+                Relationship::create([
+                    'from_id' => auth()->id(),
+                    'to_id' => $user_id,
+                    'status' => '4',
                 ]);
             }
             else {
