ddc0b22a4a0b389ffb836bdae3511f7da10b42b9
Hoangcongtue99 <hoangcongtue99@gmail.com>
2021-12-06 19:22:46 +0700

    get_analyst_result

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -8,7 +8,15 @@ use Illuminate\Http\Request;
 use App\Http\Requests\GetUserFriendRequest;
 use App\Http\Requests\GetRequestFriendRequest;
 use App\Exceptions\UserNotExistedException;
+use App\Http\Requests\GetAnalystResultRequest;
 use App\Http\Requests\SetRoleRequest;
+use App\Models\Comment;
+use App\Models\Image;
+use App\Models\Like;
+use App\Models\Message;
+use App\Models\Post;
+use Carbon\Carbon;
+use DateTime;
 
 class AdminController extends Controller
 {
@@ -16,8 +24,8 @@ class AdminController extends Controller
     {
         if (auth()->user()->role != 'admin') {
             return response()->json([
-                'code' => 1009,
-                'message' => 'Not access',
+                'code' => config('response_code.not_access'),
+                'message' => __('messages.not_access'),
             ]);
         }
         $users = User::where('role', 'user')->where('state', 'active')->skip($request->index - 1)->take($request->count)->get();
@@ -35,8 +43,8 @@ class AdminController extends Controller
     {
         if (auth()->user()->role != 'admin') {
             return response()->json([
-                'code' => 1009,
-                'message' => 'Not access',
+                'code' => config('response_code.not_access'),
+                'message' => __('messages.not_access'),
             ]);
         }
 
@@ -55,8 +63,8 @@ class AdminController extends Controller
     {
         if (auth()->user()->role != 'admin') {
             return response()->json([
-                'code' => 1009,
-                'message' => 'Not access',
+                'code' => config('response_code.not_access'),
+                'message' => __('messages.not_access'),
             ]);
         }
         $user = User::find($request->user_id);
@@ -89,8 +97,8 @@ class AdminController extends Controller
     {
         if (auth()->user()->role != 'superadmin') {
             return response()->json([
-                'code' => 1009,
-                'message' => 'Not access',
+                'code' => config('response_code.not_access'),
+                'message' => __('messages.not_access'),
             ]);
         }
 
@@ -108,5 +116,66 @@ class AdminController extends Controller
             'code' => config('response_code.ok'),
             'message' => __('messages.ok'),
         ]);
+    }
+
+    public function get_analyst_result(GetAnalystResultRequest $request)
+    {
+        // if (!in_array(auth()->user()->role, ['superadmin', 'admin'])) {
+        if (auth()->user()->role != 'superadmin') {
+            return response()->json([
+                'code' => config('response_code.not_access'),
+                'message' => __('messages.not_access'),
+            ]);
+        }
+
+        $total_users = User::count();
+        $total_messages = Message::count();
+        $total_posts = Post::count();
+        $total_likes = Like::count();
+        $total_comments = Comment::count();
+        $total_images_and_videos = Image::where('imageable_type', 'App\Models\Post')->count();
+
+        if ($request->days) {
+            $users = User::where('created_at', '>', Carbon::now()->subDays($request->days))->count();
+            $messages = Message::where('created_at', '>', Carbon::now()->subDays($request->days))->count();
+            $posts = Post::where('created_at', '>', Carbon::now()->subDays($request->days))->count();
+            $likes = Like::where('created_at', '>', Carbon::now()->subDays($request->days))->count();
+            $comments = Comment::where('created_at', '>', Carbon::now()->subDays($request->days))->count();
+            $images_and_videos = Image::where('created_at', '>', Carbon::now()->subDays($request->days))->where('imageable_type', 'App\Models\Post')->count();
+
+            return response()->json([
+                'code' => config('response_code.ok'),
+                'message' => __('messages.ok'),
+                'data' => [
+                    'users' => $users,
+                    'messages' => $messages,
+                    'posts' => $posts,
+                    'likes' => $likes,
+                    'comments' => $comments,
+                    'images_and_videos' => $images_and_videos,
+                    'total_users' => $total_users,
+                    'total_messages' => $total_messages,
+                    'total_posts' => $total_posts,
+                    'total_likes' => $total_likes,
+                    'total_comments' => $total_comments,
+                    'total_images_and_videos' => $total_images_and_videos,
+                ]
+            ]);
+        }
+
+        return response()->json([
+            'code' => config('response_code.ok'),
+            'message' => __('messages.ok'),
+            'data' => [
+                'total_users' => $total_users,
+                'total_messages' => $total_messages,
+                'total_posts' => $total_posts,
+                'total_likes' => $total_likes,
+                'total_comments' => $total_comments,
+                'total_images_and_videos' => $total_images_and_videos,
+            ]
+        ]);
+
+        
     }
 }
