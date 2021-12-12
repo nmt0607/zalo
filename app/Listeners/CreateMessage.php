47148bd570b2c9593a9d6a3ca3fb478759aceeb0
Van Vu Nguyen <nvvu6999@gmail.com>
2021-12-12 10:10:12 +0700

    Update returned data after message created

--- app/Listeners/CreateMessage.php
+++ app/Listeners/CreateMessage.php
@@ -3,8 +3,11 @@
 namespace App\Listeners;
 
 use App\Events\MessageSending;
+use App\Http\Resources\UserResource;
+use App\Models\User;
 use App\Services\ConversationService;
 use App\Services\MessageService;
+use App\Services\UserService;
 use Illuminate\Support\Facades\Redis;
 
 class CreateMessage
@@ -15,17 +18,19 @@ class CreateMessage
     /** @var ConversationService */
     protected $conversationService;
 
+    /** @var UserService */
+    protected $userService;
+
     /**
      * Create the event listener.
      *
      * @return void
      */
-    public function __construct(
-        MessageService $messageService,
-        ConversationService $conversationService
-    ) {
-        $this->messageService = $messageService;
-        $this->conversationService = $conversationService;
+    public function __construct()
+    {
+        $this->messageService = app(MessageService::class);
+        $this->conversationService = app(ConversationService::class);
+        $this->userService = app(UserService::class);
     }
 
     /**
@@ -37,16 +42,18 @@ class CreateMessage
     public function handle(MessageSending $event)
     {
         [
-            'sender' => $sender,
-            'receiver' => $receiver,
+            'sender' => ['id' => $senderId],
+            'receiver' => ['id' => $receiverId],
             'content' => $content,
         ] = $event->getData();
         $conversation = $this
             ->conversationService
-            ->findConversationByUserIds([$sender['id'], $receiver['id']]);
+            ->findConversationByUserIds([$senderId, $receiverId]);
+        $sender = User::findOrFail($senderId);
+        $receiver = User::findOrFail($receiverId);
         $message = $this->messageService->create([
-            'from_id' => $sender['id'],
-            'to_id' => $receiver['id'],
+            'from_id' => $senderId,
+            'to_id' => $receiverId,
             'message' => $content,
             'conversation_id' => $conversation->id,
             'is_read' => false,
@@ -56,8 +63,8 @@ class CreateMessage
             'event' => config('define.events.message_created'),
             'data' => [
                 'conversation_id' => $message->conversation->id,
-                'sender' => $sender,
-                'receiver' => $receiver,
+                'sender' => new UserResource($sender),
+                'receiver' => new UserResource($receiver),
                 'message_id' => $message->id,
                 'message' => $message->message,
                 'created' => $message->created_at,
